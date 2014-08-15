744fc8e23426628627f65c8469c2ce223ed77ab9
stefan.seeland@gmail.com <stefan.seeland@gmail.com>
2014-08-15 14:53:55 +0200

    added nuspec and finalized it, removed mp4box.exe from been packed two times into nupkg

--- mp4boxSharp/Properties/AssemblyInfo.cs
+++ mp4boxSharp/Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("mp4boxSharp")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Mp4BoxSharp is a wrapper for Mp4Box from http://gpac.wp.mines-telecom.fr/mp4box/")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Stefan Seeland")]
 [assembly: AssemblyProduct("mp4boxSharp")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
